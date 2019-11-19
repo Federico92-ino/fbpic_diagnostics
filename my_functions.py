758bd26ce219de4c0af96fba775af564ed4d5ed2
Federico92-ino <fede160392@hotmail.it>
2019-11-19 17:16:43 +0100

    class definition

--- my_functions.py
+++ my_functions.py
@@ -5,179 +5,196 @@ Set of functions by FA
 """
 #Import section
 import numpy as np
+from opmd_viewer import OpenPMDTimeSeries
+import json
 
+class Diag(object):
+   def __init__(self, path):
+      self.ts = OpenPMDTimeSeries(path)
+      self.params = json.load( open('params.json'))
 
-
-##########################  read_properties #########################
-def read_properties (ts,**kwargs):
-
-   """
-    Function to convert a OpenPMDTimeSeries.get_particle() array list output in a dict
-    
-    **Parameters**
-
-    ts: an OpenPMDTimeSeries object
-    **kwargs: the same parameters of .get_particle() method passed as keywords
-
-   """
-
- 
-   list=ts.get_particle(**kwargs)
-   dict={kwargs['var_list'][0]:0}
-  
-   for i,key in enumerate(kwargs['var_list']):
-      if len(list[i])==1:
-         dict[key]=float(list[i])
-      else:
-         dict[key]=list[i]
-  
-   return dict
-
-
-#####################  emittance_l  #######################
-def emittance_l (ar_list,weights):
-
-  """
-     Function to calculate bunches'normalized longitudinal emittance;
-     the result is given in mm*mrad.
-
-    **Parameters**
-     ar_list: list of two ndarrays of  phase-space coords
-     weights: ndarray of particles' weights   
-  
-    **Returns**
-     emittance, beam size and momenta spread   
-  """
- 
-
-#Check ar_list
-  try:
-    type(ar_list) == list 
-  except ValueError:
-    print('The argument must be a list')
     
 
-  if len(ar_list)>2 or len(ar_list)<1:
-    print('ar_list must be long just two strings')
-    return
+###################### read_properties #########################
+   def read_properties (**kwargs):
 
-#Longitudinal emittance
- 
- 
-  x, ux, w = ar_list[0], ar_list[1], weights
-  x_mean = np.ma.average(x, weights=w)
-  ux_mean = np.ma.average(ux, weights=w)  
-  sigma_x2 = np.ma.average((x-x_mean)**2, weights=w)
-  sigma_ux2 = np.ma.average((ux-ux_mean)**2, weights=w)
-  sigma_xux2 = (np.ma.average((x-x_mean)*(ux-ux_mean), weights=w))**2 
-  
-  emit=np.sqrt(sigma_x2*sigma_ux2-sigma_xux2)
-  return emit, np.sqrt(sigma_x2), np.sqrt(sigma_ux2)
+      """
+       Function to convert a OpenPMDTimeSeries.get_particle() array list output in a dict
 
-#########################  emittance_t  #############################
-def emittance_t (ar_list,weights):
+       **Parameters**
 
- """
-     Function to calculate bunches'normalized transverse emittance;
-     the result is given in mm*mrad.
+       ts: an OpenPMDTimeSeries object
+       **kwargs: the same parameters of .get_particle() method passed as keywords
 
-    **Parameters**
-     ar_list: list of two ndarrays of  phase-space coords
-     weights: ndarray of particles' weights
-
-    **Returns**
-     emittance, beam size, momenta spread
- """
-
-
-#Check var_list
- try:
-    type(ar_list) == list
- except ValueError:
-    print('The argument must be a list')
-
-
- if len(ar_list)>2 or len(ar_list)<1:
-    print('ar_list must be long just two strings')
-    return
-
-#Transverse emittance
-
- x, ux, w = ar_list[0], ar_list[1], weights
- 
- sigma_x2 = np.ma.average(x**2, weights=w)
- sigma_ux2 = np.ma.average(ux**2,weights=w)
- sigma_xux = np.ma.average(x*ux, weights=w)  
-
- emit=np.sqrt(sigma_x2*sigma_ux2 - sigma_xux**2)
- return emit, np.sqrt(sigma_x2), np.sqrt(sigma_ux2)
-  
-#######################  bunche_charge  ############################
-def bunch_charge (charge, weights):
- """
-   Function to calculate bunch charge
-   
-   **Parameters**
-    charge: int; single particle charge 
-    weights: ndarray of particles' weights in the bunch
-
- """
- b_charge = charge*weights.sum()
- return b_charge
-
-#####################  slice_emit  ###########################
-def slice_emit (dict, N):
-   """
-   Function to calculate slice emittances of a 'N sliced' bunch
-   **Parameters**
-      dict: a "read_properties" dictionary
-      N: int, number of slices 
-   **Returns**
-      S_prop: a dictionary of properties of each slice: emittance, size, momenta spread and phase_space of each slice
-      Ph_space: an arrays' dictionary of 'x'-'ux' values of three slices taken at z_mean, (z_mean-z_var) and (z_mean+z_var)
-      dz: longitudinal slices' thickness 
-
-   Note: here indexing of dict_keys labels over slices   
-
-   """
-   dz = (dict['z'].max()-dict['z'].min())/N
-   
-   s_emit=[]
-   s_sigma_x2=[]
-   s_sigma_ux2=[]
-   Z=[]
-   X=[]
-   UX=[]
-
-   a=dict['z'].argsort()
-   x=dict['x'][a]
-   ux=dict['ux'][a]
-   w=dict['w'][a]
-   dict['z'].sort()
+      """
 
    
-   for n in range(N):
-      inds = np.where( (dict['z'] >= dict['z'].min()+n*dz) & (dict['z'] <= dict['z'].min()+(n+1)*dz) )[0] 
+      list=self.ts.get_particle(**kwargs)
+      dict={kwargs['var_list'][0]:0}
    
-      Z.append(dict['z'][inds].mean())
+      for i,key in enumerate(kwargs['var_list']):
+         if len(list[i])==1:
+            dict[key]=float(list[i])
+         else:
+            dict[key]=list[i]
+   
+      return dict
 
-      s_prop = emittance_t( [x[inds], ux[inds]], w[inds])
-      s_emit.append(s_prop[0])
-      s_sigma_x2.append(s_prop[1])
-      s_sigma_ux2.append(s_prop[2])
+
+   #####################  emittance_l  #######################
+   def emittance_l (ar_list,weights):
+
+     """
+        Function to calculate bunches'normalized longitudinal emittance;
+        the result is given in mm*mrad.
+
+       **Parameters**
+        ar_list: list of two ndarrays of  phase-space coords
+        weights: ndarray of particles' weights   
+   
+       **Returns**
+        emittance, beam size and momenta spread   
+     """
    
 
-   S_prop={'s_emit':s_emit,'s_sigma_x2':s_sigma_x2,'s_sigma_ux2':s_sigma_ux2,'z': Z}
-   
-   for n in range(-1,1):
-      inds = np.where((dict['z'] >= dict['z'].mean()+np.sqrt((n*dict['z'].var()))-dz/2) & (dict['z'] <= dict['z'].mean()+np.sqrt((n*dict['z'].var()))+dz/2))
-      X.append(x[inds])
-      UX.append(ux[inds])
+   #Check ar_list
+     try:
+       type(ar_list) == list 
+     except ValueError:
+       print('The argument must be a list')
 
-   Ph_space = {'x': X, 'ux': UX}
 
-   return S_prop, Ph_space, dz
+     if len(ar_list)>2 or len(ar_list)<1:
+       print('ar_list must be long just two strings')
+       return
+
+   #Longitudinal emittance
    
+   
+     x, ux, w = ar_list[0], ar_list[1], weights
+     x_mean = np.ma.average(x, weights=w)
+     ux_mean = np.ma.average(ux, weights=w)  
+     sigma_x2 = np.ma.average((x-x_mean)**2, weights=w)
+     sigma_ux2 = np.ma.average((ux-ux_mean)**2, weights=w)
+     sigma_xux2 = (np.ma.average((x-x_mean)*(ux-ux_mean), weights=w))**2 
+   
+     emit=np.sqrt(sigma_x2*sigma_ux2-sigma_xux2)
+     return emit, np.sqrt(sigma_x2), np.sqrt(sigma_ux2)
+
+   #########################  emittance_t  #############################
+   def emittance_t (ar_list,weights):
+
+    """
+        Function to calculate bunches'normalized transverse emittance;
+        the result is given in mm*mrad.
+
+       **Parameters**
+        ar_list: list of two ndarrays of  phase-space coords
+        weights: ndarray of particles' weights
+
+       **Returns**
+        emittance, beam size, momenta spread
+    """
+
+
+   #Check var_list
+    try:
+       type(ar_list) == list
+    except ValueError:
+       print('The argument must be a list')
+
+
+    if len(ar_list)>2 or len(ar_list)<1:
+       print('ar_list must be long just two strings')
+       return
+
+   #Transverse emittance
+
+    x, ux, w = ar_list[0], ar_list[1], weights
+   
+    sigma_x2 = np.ma.average(x**2, weights=w)
+    sigma_ux2 = np.ma.average(ux**2,weights=w)
+    sigma_xux = np.ma.average(x*ux, weights=w)  
+
+    emit=np.sqrt(sigma_x2*sigma_ux2 - sigma_xux**2)
+    return emit, np.sqrt(sigma_x2), np.sqrt(sigma_ux2)
+   
+   #######################  bunche_charge  ############################
+   def bunch_charge (charge, weights):
+    """
+      Function to calculate bunch charge
+
+      **Parameters**
+       charge: int; single particle charge 
+       weights: ndarray of particles' weights in the bunch
+
+    """
+    b_charge = charge*weights.sum()
+    return b_charge
+
+   #####################  slice_emit  ###########################
+   def slice_emit (dict, N):
+      """
+      Function to calculate slice emittances of a 'N sliced' bunch
+      **Parameters**
+         dict: a "read_properties" dictionary
+         N: int, number of slices 
+      **Returns**
+         S_prop: a dictionary of properties of each slice: emittance, size, momenta spread and phase_space of each slice
+         Ph_space: an arrays' dictionary of 'x'-'ux' values of three slices taken at z_mean, (z_mean-z_var) and (z_mean+z_var)
+         dz: longitudinal slices' thickness 
+
+      Note: here indexing of dict_keys labels over slices   
+
+      """
+      dz = (dict['z'].max()-dict['z'].min())/N
+
+      s_emit=[]
+      s_sigma_x2=[]
+      s_sigma_ux2=[]
+      Z=[]
+      X=[]
+      UX=[]
+
+      a=dict['z'].argsort()
+      x=dict['x'][a]
+      ux=dict['ux'][a]
+      w=dict['w'][a]
+      dict['z'].sort()
+
+
+      for n in range(N):
+         inds = np.where( (dict['z'] >= dict['z'].min()+n*dz) & (dict['z'] <= dict['z'].min()+(n+1)*dz) )[0] 
+
+         Z.append(dict['z'][inds].mean())
+
+         s_prop = emittance_t( [x[inds], ux[inds]], w[inds])
+         s_emit.append(s_prop[0])
+         s_sigma_x2.append(s_prop[1])
+         s_sigma_ux2.append(s_prop[2])
+
+
+      S_prop={'s_emit':s_emit,'s_sigma_x2':s_sigma_x2,'s_sigma_ux2':s_sigma_ux2,'z': Z}
+
+      for n in range(-1,1):
+         inds = np.where((dict['z'] >= dict['z'].mean()+np.sqrt((n*dict['z'].var()))-dz/2) & (dict['z'] <= dict['z'].mean()+np.sqrt((n*dict['z'].var()))+dz/2))
+         X.append(x[inds])
+         UX.append(ux[inds])
+
+      Ph_space = {'x': X, 'ux': UX}
+
+      return S_prop, Ph_space, dz
+
+   def lineout(self, field_name, coord, iteration, theta, m, norm = False,**kwargs):
+      E, info_e = self.ts.get_field(field=field_name, coord=coord, iteration=iteration, theta=theta, m=m)
+      E0 = 1
+      Nr = self.param['Nr']
+      if norm:
+         omegap=self.params['omegap']
+         E0 = m_e *c*omeagp/e
+      plt.plot(info_e.z*1.e6,E[Nr,:]/E0,**kwargs)
+
+   def imshow(self, field, extent=info_)       
 
 
 
